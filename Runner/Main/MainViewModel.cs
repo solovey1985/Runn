fd2a3da2d608cabbbd87e5119075e98c6d5a9a25
Andrii Solovienko <andrii_solovienko@epam.com>
2017-04-26 14:18:05 +0300

    fix for RunCommand

--- Runner/Main/MainViewModel.cs
+++ Runner/Main/MainViewModel.cs
@@ -13,17 +13,47 @@ namespace Runner.ViewModels
     public class MainViewModel:BaseViewModel
     {
         IConfigurationService _configService;
-       
+        private BaseService taskRunner;
         public MainViewModel(IConfigurationService configService, RunCommand command)
         {
             _configService = configService;
             Configurations = _configService.ReadConfigurationFromFile("config.json");
-            RunCommand = command;
+        
          
         }
 
+        private TaskConfig CurrentTask { get; set; }
+
+        private BaseCommand runCommand;
+        public BaseCommand RunCommand
+        { get  {
+                return runCommand??
+                    (runCommand = new BaseCommand(obj => {
+                        Services.Models.TaskConfig current = (Services.Models.TaskConfig)obj;
+                        if (current != null)
+                            switch (current.Type)
+                            {
+                                case TaskType.Executable: 
+                                case TaskType.CommandLine:
+                                    { taskRunner = new SimpleTaskService(); break; }
+                                case TaskType.PowerShell: { taskRunner = new PowerShellService(); break; }
+                                case TaskType.Git:
+                                    {
+                                        taskRunner = new GitService();
+                                        GitTask gitTask = _configService.GetTaskById<GitTask>(current.Id);
+                                        taskRunner.Run(gitTask);
+                                        return;
+                                    }
+                                default: { taskRunner = new SimpleTaskService(); break; }
+
+                            }
+                        taskRunner.Run(current);
+                    }));
+            }
+        }
+
         public List<Services.Models.TaskConfig> Configurations { get; set; }
-        public RunCommand RunCommand { get; set; } 
+        
         
     
     }
