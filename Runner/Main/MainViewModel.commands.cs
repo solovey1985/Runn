b1e2545f7ea5cd8513078f9c69699474aa6f22e6
Andrii Solovienko <osobuy@gmail.com>
2017-05-29 00:36:58 +0300

    Task Card customization

---
+++ Runner/Main/MainViewModel.commands.cs
@@ -0,0 +1,60 @@
+﻿using Runner.Commands;
+using Runner.Services;
+using Runner.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runner.ViewModels
+{
+    public partial class MainViewModel
+    {
+        
+        private BaseCommand runCommand;
+        public BaseCommand RunCommand
+        {
+            get
+            {
+                return runCommand ??
+                    (runCommand = new BaseCommand(obj => {
+                        Services.Models.TaskConfig current = (Services.Models.TaskConfig)obj;
+                        if (current != null)
+                            switch (current.Type)
+                            {
+                                case TaskType.Executable:
+                                case TaskType.CommandLine:
+                                    { taskRunner = new SimpleTaskService(); break; }
+                                case TaskType.PowerShell: { taskRunner = new PowerShellService(); break; }
+                                case TaskType.Git:
+                                    {
+                                        taskRunner = new GitService();
+                                        GitTask gitTask = _configService.GetTaskById<GitTask>(current.Id);
+                                        taskRunner.Run(gitTask);
+                                        return;
+                                    }
+                                default: { taskRunner = new SimpleTaskService(); break; }
+
+                            }
+                        taskRunner.Run(current);
+                    }));
+            }
+        }
+
+        private BaseCommand editCommand;
+        public BaseCommand EditCommand
+        {
+            get
+            {
+                return editCommand ??
+                    (editCommand = new BaseCommand(obj => {
+                        #region Command functionality
+                        
+
+                        #endregion
+                    }));
+            }
+        }
+    }
+}
--- Runner/Main/MainViewModel.cs
+++ Runner/Main/MainViewModel.cs
@@ -10,46 +10,18 @@ using Runner.Services;
 
 namespace Runner.ViewModels
 {
-    public class MainViewModel:BaseViewModel
+    public partial class MainViewModel:BaseViewModel
     {
         IConfigurationService _configService;
         private BaseService taskRunner;
-        public MainViewModel(IConfigurationService configService, RunCommand command)
+        public MainViewModel(IConfigurationService configService)
         {
             _configService = configService;
             Configurations = _configService.ReadConfigurationFromFile("config.json");
         }
 
         private TaskConfig CurrentTask { get; set; }
-
-        private BaseCommand runCommand;
-        public BaseCommand RunCommand
-        { get  {
-                return runCommand??
-                    (runCommand = new BaseCommand(obj => {
-                        Services.Models.TaskConfig current = (Services.Models.TaskConfig)obj;
-                        if (current != null)
-                            switch (current.Type)
-                            {
-                                case TaskType.Executable: 
-                                case TaskType.CommandLine:
-                                    { taskRunner = new SimpleTaskService(); break; }
-                                case TaskType.PowerShell: { taskRunner = new PowerShellService(); break; }
-                                case TaskType.Git:
-                                    {
-                                        taskRunner = new GitService();
-                                        GitTask gitTask = _configService.GetTaskById<GitTask>(current.Id);
-                                        taskRunner.Run(gitTask);
-                                        return;
-                                    }
-                                default: { taskRunner = new SimpleTaskService(); break; }
-
-                            }
-                        taskRunner.Run(current);
-                    }));
-            }
-        }
-
+        
         public List<Services.Models.TaskConfig> Configurations { get; set; }
         
         
